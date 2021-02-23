6494be138125c52009ac0e4c4a570a591e0de62e
Jonathan Rivera <johny24rivera@outlook.com>
2021-02-22 19:47:49 -0700

    Created game Schema

---
+++ models/game.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+
+const gameSchema = mongoose.Schema({
+    questions: [{
+        questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Question', required: true }
+    }],
+    score: {
+        type: Number,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+
+});
+
+
+module.exports = mongoose.model('Game', userSchema);
