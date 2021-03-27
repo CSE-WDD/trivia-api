80b870013d071c2a3c5353b3609cfd38a93d22c4
unknown <jonathan24rivera@gmail.com>
2021-03-27 01:14:45 -0700

    added edit question method

--- controllers/question.js
+++ controllers/question.js
@@ -1,18 +1,20 @@
-exports.postQuestion = (req, res, next) => {
-  const question = new Question(req.body);
+const Question = require('../models/question');
 
-  question.save((err, doc) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).json({
-        success: false,
-      });
-    }
-    res.status(200).json({
-      success: true,
-      question: doc,
+exports.postQuestion = (req, res, next) => {
+    const question = new Question(req.body);
+
+    question.save((err, doc) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).json({
+                success: false,
+            });
+        }
+        res.status(200).json({
+            success: true,
+            question: doc,
+        });
     });
-  });
 };
 
 exports.getQuestion = (req, res, next) => {
@@ -21,3 +23,29 @@ exports.getQuestion = (req, res, next) => {
         id: req.user._id,
     })
 };
+
+exports.editQuestion = (req, res, next) => {
+    const questionId = req.body.questionId
+    const question = req.body.question;
+    const answers = req.body.answers;
+    const category = req.body.category;
+    const difficulty = req.body.difficulty;
+
+    Question.findById(questionId).then(q => {
+        q.question = question;
+        q.answers = answers;
+        q.category = category;
+        q.difficulty = difficulty;
+    }).then(result => {
+        console.log(result);
+        res.status(200).json({
+            success: true,
+        })
+    }).catch(err => {
+        console.log(err);
+        res.status(400).json({
+            success: false,
+        })
+    })
+
+}
