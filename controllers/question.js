012e835c65dbd943730ed4e825ef2f1d69a7cc20
Jonathan Rivera <johny24rivera@outlook.com>
2021-03-30 01:45:08 -0600

    delete question added

--- controllers/question.js
+++ controllers/question.js
@@ -1,23 +1,40 @@
 exports.postQuestion = (req, res, next) => {
-  const question = new Question(req.body);
+    const question = new Question(req.body);
 
-  question.save((err, doc) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).json({
-        success: false,
-      });
-    }
-    res.status(200).json({
-      success: true,
-      question: doc,
+    question.save((err, doc) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).json({
+                success: false,
+            });
+        }
+        res.status(200).json({
+            success: true,
+            question: doc,
+        });
     });
-  });
 };
 
 exports.getQuestion = (req, res, next) => {
-  res.json({
-    isAuth: true,
-    id: req.user._id,
-  });
+    res.json({
+        isAuth: true,
+        id: req.user._id,
+    });
+};
+
+exports.deleteQuestion = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findByIdAndRemove(prodId)
+        .then(results => { console.log(results) })
+        .then(() => {
+            res.status(200).json({
+                success: true,
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json({
+                success: false,
+            });
+        });
 };
