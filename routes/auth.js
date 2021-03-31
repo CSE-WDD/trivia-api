fe545a0194493037c91a606d8cf2d80173b41374
Ian Petty <ipetty@masonite.com>
2021-03-31 17:25:01 -0400

    PRO-5 Delete a user account

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const {
-    postSignup,
-    postLogin,
-    getProfile,
-    getLogout,
-    getUserScores
+  postSignup,
+  postLogin,
+  getProfile,
+  getLogout,
+  getUserScores,
+  deleteUser,
 } = require('../controllers/auth');
 
 const {
-    postQuestion,
-    editQuestion,
-    deleteQuestion,
-    getAllQuestions
+  postQuestion,
+  editQuestion,
+  deleteQuestion,
+  getAllQuestions,
 } = require('../controllers/question');
 
-const {
-    auth,
-    authAdmin,
-} = require('../middlewares/auth');
+const { auth, authAdmin } = require('../middlewares/auth');
 
 router.post('/api/register', postSignup);
 router.post('/api/login', postLogin);
 router.get('/api/profile', auth, getProfile);
 router.get('/api/logout', auth, getLogout);
+router.post('/api/deleteUser', auth, deleteUser);
 router.get('/api/get-user-scores', auth, getUserScores);
 router.get('/api/get-all-questions', authAdmin, getAllQuestions);
 router.post('/api/submit-question', authAdmin, postQuestion);
