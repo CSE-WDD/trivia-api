8b40014fd74d727417e4ce5fdb34b0439d627258
matthewrapp <mattrapp25@gmail.com>
2021-04-03 14:50:36 -0700

    credentials: true

--- index.js
+++ index.js
@@ -36,21 +36,22 @@ app.get('/', function (req, res) {
   res.status(200).send({ message: 'Welcome to login , sign-up api' });
 });
 
-app.use((req, res, next) => {
-  // '*' allows all clients to access the server through the API
-  // Otherwise, you can allow only certain domains by placing them in, using commas to separate
-  // res.setHeader('Access-Control-Allow-Origin', 'codepen.io');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   // '*' allows all clients to access the server through the API
+//   // Otherwise, you can allow only certain domains by placing them in, using commas to separate
+//   // res.setHeader('Access-Control-Allow-Origin', 'codepen.io');
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+//   );
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 const corsOptions = {
-  origin: '*'
+  origin: '*',
+  credentials: true
 };
 app.use(cors(corsOptions));
 
