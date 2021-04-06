65bc930c27a5d8625b5de3b5e5a87a448d3db618
matthewrapp <mattrapp25@gmail.com>
2021-04-05 19:10:51 -0600

    cookie should work

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,8 @@
 const User = require('./../models/user');
 
 let auth = (req, res, next) => {
-  let token = req.cookies.auth;
+  let token = req.cookies.auth !== undefined ? req.cookies.auth : req.body.token;
+  
   User.findByToken(token, (err, user) => {
     if (err) throw err;
     if (!user)
@@ -16,7 +17,8 @@ let auth = (req, res, next) => {
 };
 
 let authAdmin = (req, res, next) => {
-  let token = req.cookies.auth;
+  let token = req.cookies.auth !== undefined ? req.cookies.auth : req.body.token;
+
   User.findByToken(token, (err, user) => {
     if (err) throw err;
     if (!user)
